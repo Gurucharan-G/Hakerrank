27674ae5a68f64e7ba63ca0d9c259bb9c6220d1d
Gurucharan-G <gurucharan9499@gmail.com>
2023-05-14 19:32:01 +0530

    Merge conflict

--- Hello_World.py
+++ Hello_World.py
@@ -1,8 +1,21 @@
-#Method one -- Direct Print
+"""Given an integer, , perform the following conditional actions:
 
-print("Hello World")
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Input Format
 
-#Method two -- Store in variable then print 
+A single line containing a positive integer, .
 
-msg="Hello World"
-print(msg)
+Constraints
+
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird."""
+
+#Method 1
+n= int(input().strip())
+if(1<=n<=100):
+    if (n%2==0 and ((2<=n<=5)or(n>20))): print("Not Weird")
+    elif(n%2!=0 or (6<=n<=20)):print("Weird")
